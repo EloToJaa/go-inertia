9f52a48be6d2bcf519000687dca6950806b3a6f8
EloToJaa <elotoja@protonmail.com>
2025-04-26 03:33:52 +0200

    Remove unused ssr config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,20 +18,20 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  // build: {
-  //   manifest: true, // Generate manifest.json file
-  //   outDir: "public/build",
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     input: "src/app.ts",
-  //     output: {
-  //       entryFileNames: "assets/[name].js",
-  //       chunkFileNames: "assets/[name].js",
-  //       assetFileNames: "assets/[name].[ext]",
-  //       manualChunks: undefined, // Disable automatic chunk splitting
-  //     },
-  //   },
-  // },
+  build: {
+    manifest: true, // Generate manifest.json file
+    outDir: "public/build",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: "src/app.ts",
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+        manualChunks: undefined, // Disable automatic chunk splitting
+      },
+    },
+  },
   server: {
     hmr: {
       host: "localhost",
